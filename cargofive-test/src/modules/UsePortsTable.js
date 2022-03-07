6e49081d6ed2f415cd771428a25307a0a188b421
Samuel Bermudez <sam93quimica14@gmail.com>
2022-03-07 12:57:00 -0400

    cargofive-test: some coments

--- cargofive-test/src/modules/UsePortsTable.js
+++ cargofive-test/src/modules/UsePortsTable.js
@@ -1,10 +1,14 @@
 import { reactive } from "vue";
 import axios from "axios";
 
+// valores de estado
 const state = reactive({
+  // ports es para guardar los puertos "en bruto"
   ports: [],
+  // sortedPorts es los puertos ordenados
   sortedPorts:[],
   headers:[],
+  // este es el array que se usa en la tabla, es un array de arrays de objetos
   paginationPorts:[],
   pageOn: 1,
   pages:0,
@@ -12,7 +16,7 @@ const state = reactive({
 })
 
 const loader ={
-
+  // funcion para consumir la api y guardarlo en ports
   async getPorts(){
     try{
       const {data:{data}} = await axios.get()
@@ -22,14 +26,15 @@ const loader ={
       console.log('error',e)
     }
   },
-
+  // funcion para obtener el titulo de las columnas de la tabla
   itemHeaders() {
     const keys = state.ports[0]? Object.keys(state.ports[0]).filter(item=> item !='id'): '';
     const keysCapitalized = keys.map( key => ({ name: key.charAt(0).toUpperCase() + key.slice(1), active: key==='name'?true:false}))
     state.headers= keysCapitalized
     loader.sortPorts('Name')
   },
-
+  // funcion para separar los el array de los puertos ordenas y/o filtrados
+  // en partes iguales para la paginacion
   separatedPorts(portsToSeparate = state.sortedPorts){
     let chunks = [],
         i = 0,
@@ -45,11 +50,15 @@ const loader ={
     state.pageOn=1;
   },
 
+  // funcion en caso de que se borre el mensaje del filtro
   clearMessage(){
     state.filter = ''
     loader.separatedPorts()
   },
 
+  // funcion para filtrar el array de puertos ordenados
+  // revisa en todas las columnas que valor es igual al escrito en el filtro
+  // y devuelve un array filtrado para ser separado
   filterData(){
     if (state.filter){
       const portsFiltred = state.sortedPorts.filter(port =>{
@@ -69,6 +78,9 @@ const loader ={
     }
   },
 
+  // siempre se ordenan los puertos, en este caso siempre se ordena primeramente por el Nombre
+  // pero nunca se ordena por coordenadas
+  // como estableces un orden para las coordenadas?
   sortPorts( orderBy ){
     if( orderBy !== 'Coordinates'){
       const sortedPorts = state.ports.sort((portA,portB)=>{
@@ -84,6 +96,7 @@ const loader ={
     }
   }
 }
+
 export default ({
   state,
   loader
